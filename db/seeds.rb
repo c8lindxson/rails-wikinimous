a1e66b877394edff8ccbdef6308c2b9839950882
Cai Dixon <city.art.girl@gmail.com>
2023-05-09 15:51:44 +0900

    speed run build with minimal style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require 'faker'
+
+10.times do
+  new_title = Faker::Marketing.buzzwords
+  new_content = Faker::Lorem.paragraph
+  Article.create(title: new_title, content: new_content)
+end
